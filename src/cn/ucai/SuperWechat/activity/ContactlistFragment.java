68957eb9a7dad60737f3f16e2cfda770a3d63301
阿木木 <454700740@qq.com>
2016-07-24 18:15:01 +0800

    更新添加好友后的联系人信息

--- src/cn/ucai/SuperWechat/activity/ContactlistFragment.java
+++ src/cn/ucai/SuperWechat/activity/ContactlistFragment.java
@@ -22,8 +22,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -268,6 +270,7 @@ public class ContactlistFragment extends Fragment {
 		} else {
 			progressBar.setVisibility(View.GONE);
 		}
+		setMupdateContactReceiver();
 	}
 
 	@Override
@@ -497,5 +500,26 @@ public class ContactlistFragment extends Fragment {
 	    	outState.putBoolean(Constant.ACCOUNT_REMOVED, true);
 	    }
 	    
+	}
+
+	class UpdateContactReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			adapter.notifyDataSetChanged();
+		}
+	}
+
+	UpdateContactReceiver mupdateContactReceiver;
+	private void setMupdateContactReceiver(){
+		mupdateContactReceiver = new UpdateContactReceiver();
+		IntentFilter filter = new IntentFilter("update_contact_list");
+		getActivity().registerReceiver(mupdateContactReceiver, filter);
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		getActivity().unregisterReceiver(mupdateContactReceiver);
 	}
 }

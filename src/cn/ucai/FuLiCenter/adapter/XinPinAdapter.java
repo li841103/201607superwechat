29e48671450b1f3790891154fa2ac31b5a1d08d3
阿木木 <454700740@qq.com>
2016-08-02 21:25:28 +0800

    依据商品的上架时间进行排序

--- src/cn/ucai/FuLiCenter/adapter/XinPinAdapter.java
+++ src/cn/ucai/FuLiCenter/adapter/XinPinAdapter.java
@@ -9,10 +9,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import cn.ucai.FuLiCenter.R;
 import cn.ucai.FuLiCenter.bean.NewGoodBean;
 import cn.ucai.FuLiCenter.utils.ImageUtils;
+import cn.ucai.FuLiCenter.widget.I;
 
 
 /**
@@ -22,6 +25,12 @@ public class XinPinAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     Context mcontext;
     List<NewGoodBean> mList;
     XinPinViewHolder mXinPinViewHolder;
+    FootViewHolder mFootViewHolder;
+    String footer;
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
 
     public XinPinAdapter(Context mcontext, List<NewGoodBean> mList) {
         this.mcontext = mcontext;
@@ -32,9 +41,24 @@ public class XinPinAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         RecyclerView.ViewHolder holder = null;
-        View view = LayoutInflater.from(mcontext).inflate(R.layout.xinpin_item, null, false);
-        holder = new XinPinViewHolder(view);
+        if(viewType==I.TYPE_FOOTER){
+            View view = LayoutInflater.from(mcontext).inflate(R.layout.foot_item, null, false);
+            holder = new FootViewHolder(view);
+        }
+        if(viewType==I.TYPE_ITEM){
+            View view = LayoutInflater.from(mcontext).inflate(R.layout.xinpin_item, null, false);
+            holder = new XinPinViewHolder(view);
+        }
         return holder;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(position==getItemCount()-1){
+            return I.TYPE_FOOTER;
+        }else{
+            return I.TYPE_ITEM;
+        }
     }
 
     @Override
@@ -46,18 +70,28 @@ public class XinPinAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             mXinPinViewHolder.mmtvDesc.setText(xin.getGoodsBrief());
             ImageUtils.setXinPinImage(mcontext,xin.getGoodsThumb(),mXinPinViewHolder.mmivImage);
         }
+        if(holder instanceof FootViewHolder){
+            mFootViewHolder = (FootViewHolder) holder;
+            mFootViewHolder.mfoothint.setText(footer);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mList==null?0:mList.size();
+        return mList==null?1:mList.size()+1;
     }
 
-    public void initData(ArrayList<NewGoodBean> arr) {
-        if(mList!=null){
+    public void initData(ArrayList<NewGoodBean> arr,int DOWN_CODE) {
+        if(mList!=null&&DOWN_CODE==0){
             Log.i("main", "mList!=null");
             mList.clear();
             mList.addAll(arr);
+            soryByAddTime();
+            notifyDataSetChanged();
+        }
+        if(mList!=null&&DOWN_CODE==1){
+            mList.addAll(arr);
+            soryByAddTime();
             notifyDataSetChanged();
         }
     }
@@ -72,6 +106,25 @@ public class XinPinAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             mmivImage = (ImageView) itemView.findViewById(R.id.ivImage);
             mmtvMoney = (TextView) itemView.findViewById(R.id.tvMoney);
         }
+    }
+
+    class FootViewHolder extends RecyclerView.ViewHolder{
+        TextView mfoothint;
+
+        public FootViewHolder(View itemView) {
+            super(itemView);
+            mfoothint = (TextView) itemView.findViewById(R.id.foot_hint);
+
+        }
+    }
+
+    private void soryByAddTime(){
+        Collections.sort(mList, new Comparator<NewGoodBean>() {
+            @Override
+            public int compare(NewGoodBean left, NewGoodBean right) {
+                return (int) (left.getAddTime()-right.getAddTime());
+            }
+        });
     }
 
 }

cc4e7e35540758083ff2e03d18963b28086ace3b
阿木木 <454700740@qq.com>
2016-08-08 20:37:08 +0800

    收藏宝贝数量显示

--- src/cn/ucai/FuLiCenter/activity/PersonalCenterFragment.java
+++ src/cn/ucai/FuLiCenter/activity/PersonalCenterFragment.java
@@ -1,7 +1,9 @@
 package cn.ucai.FuLiCenter.activity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,7 +15,9 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import cn.ucai.FuLiCenter.DemoHXSDKHelper;
+import cn.ucai.FuLiCenter.FuLiCenterApplication;
 import cn.ucai.FuLiCenter.R;
+import cn.ucai.FuLiCenter.utils.UserUtils;
 
 /**
  * Created by Zhou on 2016/8/8.
@@ -21,7 +25,7 @@ import cn.ucai.FuLiCenter.R;
 public class PersonalCenterFragment extends Fragment{
     Context mContext;
     ImageView iv_msg,iv_avatar;
-    TextView tv_sttings,tv_username;
+    TextView tv_sttings,tv_username,shoucangNum;
     RelativeLayout rl_soucang_baobei,rl_soucang_dianpu,rl_zhuji,My_dingdan;
     @Nullable
     @Override
@@ -29,17 +33,27 @@ public class PersonalCenterFragment extends Fragment{
         mContext = (FuLiCenterActivity) getContext();
         View layout = View.inflate(mContext, R.layout.personal_data, null);
         initView(layout);
+        initData();
         setListener();
         return layout;
+    }
+
+    private void initData() {
+        if(DemoHXSDKHelper.getInstance().isLogined()){
+            UserUtils.setAppCurrentUserNick(tv_username);
+            UserUtils.setAppCurrentUserAvatar(mContext,iv_avatar);
+        }
     }
 
     private void setListener() {
         setListenerclass listener = new setListenerclass();
         iv_avatar.setOnClickListener(listener);
         tv_sttings.setOnClickListener(listener);
+        updateCollectCountListener();
     }
 
     private void initView(View layout) {
+        shoucangNum = (TextView) layout.findViewById(R.id.shoucangNum);
         tv_sttings= (TextView) layout.findViewById(R.id.tv_sttings);
         iv_msg = (ImageView) layout.findViewById(R.id.iconfont_xinxi);
         iv_avatar = (ImageView) layout.findViewById(R.id.default_avatar);
@@ -63,5 +77,24 @@ public class PersonalCenterFragment extends Fragment{
         }
     }
 
+    class UpdateCollectCount extends BroadcastReceiver{
 
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int collectcount = FuLiCenterApplication.getInstance().getCollectcount();
+            shoucangNum.setText(String.valueOf(collectcount));
+        }
+    }
+    UpdateCollectCount mReceiver;
+    private void updateCollectCountListener(){
+        mReceiver = new UpdateCollectCount();
+        IntentFilter filter = new IntentFilter("update_contact");
+        mContext.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mContext.unregisterReceiver(mReceiver);
+    }
 }
